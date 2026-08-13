18c135c9a8de5b7d622b90ce921546b44198a029
Vanesa Robledo <v.robledo@vehikl.com>
2026-08-13 15:42:14 -0400

    add one draggable Mahjomg tile

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {UseDraggable as Draggable} from '@vueuse/components'
+
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Mahjong Test</h1>
+  <Draggable
+      p="x-4 y-2"
+      style="position: fixed;"
+      :initial-value="{ x: 10, y: 60}"
+      :prevent-default="true"
+  >
+    <img class="tile" src="/img/tiles/Pin1.svg" alt="Pin 1 tile"/>
+  </Draggable>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+.tile {
+  width: 25px;
+  height: 36px;
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  padding: 5px;
+}
+</style>
