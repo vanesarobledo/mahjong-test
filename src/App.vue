138b9116837cb8cc1492c3f4af8b3a04e4b2f041
Vanesa Robledo <v.robledo@vehikl.com>
2026-08-13 16:12:01 -0400

    now there are two draggable tiles!!!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <script setup lang="ts">
 import {UseDraggable as Draggable} from '@vueuse/components'
+import {ref} from "vue";
 
+const tiles = ref(
+    [
+      {
+        id: 0,
+        src: '/img/tiles/Pin1.svg',
+      },
+      {
+        id: 1,
+        src: '/img/tiles/Pin2.svg',
+      }
+    ]
+)
+
+// computed property for positions
 
 </script>
 
 <template>
   <h1>Mahjong Test</h1>
+
   <Draggable
-      p="x-4 y-2"
-      style="position: fixed;"
+      v-for="tile in tiles"
+      :key="tile.id"
+      class="tile"
       :initial-value="{ x: 10, y: 60}"
       :prevent-default="true"
   >
-    <img class="tile" src="/img/tiles/Pin1.svg" alt="Pin 1 tile"/>
+    <img :src="tile.src" alt="tile" />
   </Draggable>
 
 </template>
 
 <style scoped>
 .tile {
+  position: fixed;
   width: 25px;
   height: 36px;
   border: 1px solid #ccc;
   border-radius: 2px;
   padding: 5px;
+}
+.tile img {
+  width: 100%;
 }
 </style>
