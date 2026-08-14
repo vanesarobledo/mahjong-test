002ad770f65dbb08d06fdc0644ece08c0ee309a8
Vanesa Robledo <v.robledo@vehikl.com>
2026-08-14 00:38:47 -0400

    allow tiles to be flipped

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import { UseDraggable as Draggable } from '@vueuse/components'
-import { ref } from "vue";
+import { ref, useTemplateRef } from "vue";
 import { Tiles } from '@/data/tiles.js'
+import TileDraggable from "@/components/TileDraggable.vue";
 
-const tiles = ref(Tiles)
+const boardRef = useTemplateRef('board')
+const tiles = ref(
+    Tiles.map(t => (
+        {
+          ...t,
+          rotationY: 0,
+          isFaceUp: true
+        }
+    ))
+)
 
 const TILE_SPACING_X = 30
 const TILE_SPACING_Y = 45
@@ -18,35 +27,81 @@ function getInitializedPosition(index : number) {
   }
 }
 
+function flipTile(tile) {
+  tile.isFaceUp = !tile.isFaceUp
+  tile.rotationY = tile.isFaceUp ? 0: 180
+}
+
 </script>
 
 <template>
   <h1>Mahjong Test</h1>
 
-  <div style="position: relative";>
-    <Draggable
+  <div ref="board" class="board">
+    <TileDraggable
         v-for="(tile, index) in tiles"
         :key="tile.id"
         class="tile"
-        :initial-value="getInitializedPosition(index)"
+        :initial-x="getInitializedPosition(index).x"
+        :initial-y="getInitializedPosition(index).y"
         :prevent-default="true"
+        @click="flipTile(tile)"
+        :container-ref="boardRef"
     >
-      <img :src="tile.src" alt="tile" />
-    </Draggable>
+      <div
+          class="tile-3d"
+          :style="{ transform: `rotateY(${tile.rotationY}deg)` }"
+      >
+        <div class="face front">
+          <img :src="tile.src" :alt="tile.name" />
+        </div>
+        <div class="face back"></div>
+      </div>
+    </TileDraggable>
   </div>
 
 </template>
 
 <style scoped>
+.board {
+  position: relative;
+  perspective: 800px;
+  min-height: 100vh;
+}
 .tile {
-  position: fixed;
+  position: absolute;
   width: 25px;
   height: 36px;
-  border-radius: 3px;
-  padding: 5px;
-  background: center / contain no-repeat url('/img/tiles/Front.svg');
 }
-.tile img {
+.tile-3d {
+  position: relative;
   width: 100%;
+  height: 100%;
+  transform-style: preserve-3d;
+  transition: transform 0.4s;
+  cursor: pointer;
+}
+.face {
+  position: absolute;
+  inset: 0;
+  border-radius: 3px;
+  background: center / contain no-repeat;
+  backface-visibility: hidden;
+  box-shadow: 1px 1px rgba(0, 0, 0, 0.3);
+}
+
+.front {
+  background-image: url('/img/tiles/Front.svg');
+  padding: 5px;
+}
+
+.front img {
+  width: 100%;
+  height: 100%;
+}
+
+.back {
+  background-image: url('/img/tiles/Back.svg');
+  transform: rotateY(180deg);
 }
 </style>
