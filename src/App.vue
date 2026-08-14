049d8cca0cfe026befafbfead6ee174ab01f2aec
Vanesa Robledo <v.robledo@vehikl.com>
2026-08-13 22:49:59 -0400

    display all Mahjong tiles

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <script setup lang="ts">
-import {UseDraggable as Draggable} from '@vueuse/components'
-import {ref} from "vue";
-import {Tiles} from '@/data/tiles.js'
+import { UseDraggable as Draggable } from '@vueuse/components'
+import { ref } from "vue";
+import { Tiles } from '@/data/tiles.js'
 
 const tiles = ref(Tiles)
 
-// computed property for positions
+const TILE_SPACING_X = 30
+const TILE_SPACING_Y = 45
+const TILES_PER_ROW = 14
+
+function getInitializedPosition(index : number) {
+  const col = index % TILES_PER_ROW;
+  const row = Math.floor(index / TILES_PER_ROW);
+  return {
+    x: 10 + col * TILE_SPACING_X,
+    y: 60 + row * TILE_SPACING_Y
+  }
+}
 
 </script>
 
 <template>
   <h1>Mahjong Test</h1>
 
-  <div style="display: flex">
-  <Draggable
-      v-for="tile in tiles"
-      :key="tile.id"
-      class="tile"
-      :initial-value="{ x: 10, y: 60}"
-      :prevent-default="true"
-  >
-    <img :src="tile.src" alt="tile" />
-  </Draggable>
+  <div style="position: relative";>
+    <Draggable
+        v-for="(tile, index) in tiles"
+        :key="tile.id"
+        class="tile"
+        :initial-value="getInitializedPosition(index)"
+        :prevent-default="true"
+    >
+      <img :src="tile.src" alt="tile" />
+    </Draggable>
   </div>
 
 </template>
