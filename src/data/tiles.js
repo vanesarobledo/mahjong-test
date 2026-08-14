1c46eacfe3e92d83f56dd3616623fdc7eba4205c
Vanesa Robledo <v.robledo@vehikl.com>
2026-08-13 22:49:48 -0400

    replicate standard riichi mahjong set

--- src/data/tiles.js
+++ src/data/tiles.js
@@ -1,39 +1,682 @@
 export const Tiles = [
-  { "id": 1, "src": "/img/tiles/Chun.svg" },
-  { "id": 2, "src": "/img/tiles/Haku.svg" },
-  { "id": 3, "src": "/img/tiles/Hatsu.svg" },
-  { "id": 4, "src": "/img/tiles/Man1.svg" },
-  { "id": 5, "src": "/img/tiles/Man2.svg" },
-  { "id": 6, "src": "/img/tiles/Man3.svg" },
-  { "id": 7, "src": "/img/tiles/Man4.svg" },
-  { "id": 8, "src": "/img/tiles/Man5-Dora.svg" },
-  { "id": 9, "src": "/img/tiles/Man5.svg" },
-  { "id": 10, "src": "/img/tiles/Man6.svg" },
-  { "id": 11, "src": "/img/tiles/Man7.svg" },
-  { "id": 12, "src": "/img/tiles/Man8.svg" },
-  { "id": 13, "src": "/img/tiles/Man9.svg" },
-  { "id": 14, "src": "/img/tiles/Nan.svg" },
-  { "id": 15, "src": "/img/tiles/Pei.svg" },
-  { "id": 16, "src": "/img/tiles/Pin1.svg" },
-  { "id": 17, "src": "/img/tiles/Pin2.svg" },
-  { "id": 18, "src": "/img/tiles/Pin3.svg" },
-  { "id": 19, "src": "/img/tiles/Pin4.svg" },
-  { "id": 20, "src": "/img/tiles/Pin5-Dora.svg" },
-  { "id": 21, "src": "/img/tiles/Pin5.svg" },
-  { "id": 22, "src": "/img/tiles/Pin6.svg" },
-  { "id": 23, "src": "/img/tiles/Pin7.svg" },
-  { "id": 24, "src": "/img/tiles/Pin8.svg" },
-  { "id": 25, "src": "/img/tiles/Pin9.svg" },
-  { "id": 26, "src": "/img/tiles/Shaa.svg" },
-  { "id": 27, "src": "/img/tiles/Sou1.svg" },
-  { "id": 28, "src": "/img/tiles/Sou2.svg" },
-  { "id": 29, "src": "/img/tiles/Sou3.svg" },
-  { "id": 30, "src": "/img/tiles/Sou4.svg" },
-  { "id": 31, "src": "/img/tiles/Sou5-Dora.svg" },
-  { "id": 32, "src": "/img/tiles/Sou5.svg" },
-  { "id": 33, "src": "/img/tiles/Sou6.svg" },
-  { "id": 34, "src": "/img/tiles/Sou7.svg" },
-  { "id": 35, "src": "/img/tiles/Sou8.svg" },
-  { "id": 36, "src": "/img/tiles/Sou9.svg" },
-  { "id": 37, "src": "/img/tiles/Ton.svg" }
+  {
+    "id": 0,
+    "src": "/img/tiles/Man1.svg",
+    "name": "Man1"
+  },
+  {
+    "id": 1,
+    "src": "/img/tiles/Man1.svg",
+    "name": "Man1"
+  },
+  {
+    "id": 2,
+    "src": "/img/tiles/Man1.svg",
+    "name": "Man1"
+  },
+  {
+    "id": 3,
+    "src": "/img/tiles/Man1.svg",
+    "name": "Man1"
+  },
+  {
+    "id": 4,
+    "src": "/img/tiles/Man2.svg",
+    "name": "Man2"
+  },
+  {
+    "id": 5,
+    "src": "/img/tiles/Man2.svg",
+    "name": "Man2"
+  },
+  {
+    "id": 6,
+    "src": "/img/tiles/Man2.svg",
+    "name": "Man2"
+  },
+  {
+    "id": 7,
+    "src": "/img/tiles/Man2.svg",
+    "name": "Man2"
+  },
+  {
+    "id": 8,
+    "src": "/img/tiles/Man3.svg",
+    "name": "Man3"
+  },
+  {
+    "id": 9,
+    "src": "/img/tiles/Man3.svg",
+    "name": "Man3"
+  },
+  {
+    "id": 10,
+    "src": "/img/tiles/Man3.svg",
+    "name": "Man3"
+  },
+  {
+    "id": 11,
+    "src": "/img/tiles/Man3.svg",
+    "name": "Man3"
+  },
+  {
+    "id": 12,
+    "src": "/img/tiles/Man4.svg",
+    "name": "Man4"
+  },
+  {
+    "id": 13,
+    "src": "/img/tiles/Man4.svg",
+    "name": "Man4"
+  },
+  {
+    "id": 14,
+    "src": "/img/tiles/Man4.svg",
+    "name": "Man4"
+  },
+  {
+    "id": 15,
+    "src": "/img/tiles/Man4.svg",
+    "name": "Man4"
+  },
+  {
+    "id": 16,
+    "src": "/img/tiles/Man5.svg",
+    "name": "Man5"
+  },
+  {
+    "id": 17,
+    "src": "/img/tiles/Man5.svg",
+    "name": "Man5"
+  },
+  {
+    "id": 18,
+    "src": "/img/tiles/Man5.svg",
+    "name": "Man5"
+  },
+  {
+    "id": 19,
+    "src": "/img/tiles/Man5-Dora.svg",
+    "name": "Man5-Dora"
+  },
+  {
+    "id": 20,
+    "src": "/img/tiles/Man6.svg",
+    "name": "Man6"
+  },
+  {
+    "id": 21,
+    "src": "/img/tiles/Man6.svg",
+    "name": "Man6"
+  },
+  {
+    "id": 22,
+    "src": "/img/tiles/Man6.svg",
+    "name": "Man6"
+  },
+  {
+    "id": 23,
+    "src": "/img/tiles/Man6.svg",
+    "name": "Man6"
+  },
+  {
+    "id": 24,
+    "src": "/img/tiles/Man7.svg",
+    "name": "Man7"
+  },
+  {
+    "id": 25,
+    "src": "/img/tiles/Man7.svg",
+    "name": "Man7"
+  },
+  {
+    "id": 26,
+    "src": "/img/tiles/Man7.svg",
+    "name": "Man7"
+  },
+  {
+    "id": 27,
+    "src": "/img/tiles/Man7.svg",
+    "name": "Man7"
+  },
+  {
+    "id": 28,
+    "src": "/img/tiles/Man8.svg",
+    "name": "Man8"
+  },
+  {
+    "id": 29,
+    "src": "/img/tiles/Man8.svg",
+    "name": "Man8"
+  },
+  {
+    "id": 30,
+    "src": "/img/tiles/Man8.svg",
+    "name": "Man8"
+  },
+  {
+    "id": 31,
+    "src": "/img/tiles/Man8.svg",
+    "name": "Man8"
+  },
+  {
+    "id": 32,
+    "src": "/img/tiles/Man9.svg",
+    "name": "Man9"
+  },
+  {
+    "id": 33,
+    "src": "/img/tiles/Man9.svg",
+    "name": "Man9"
+  },
+  {
+    "id": 34,
+    "src": "/img/tiles/Man9.svg",
+    "name": "Man9"
+  },
+  {
+    "id": 35,
+    "src": "/img/tiles/Man9.svg",
+    "name": "Man9"
+  },
+  {
+    "id": 36,
+    "src": "/img/tiles/Pin1.svg",
+    "name": "Pin1"
+  },
+  {
+    "id": 37,
+    "src": "/img/tiles/Pin1.svg",
+    "name": "Pin1"
+  },
+  {
+    "id": 38,
+    "src": "/img/tiles/Pin1.svg",
+    "name": "Pin1"
+  },
+  {
+    "id": 39,
+    "src": "/img/tiles/Pin1.svg",
+    "name": "Pin1"
+  },
+  {
+    "id": 40,
+    "src": "/img/tiles/Pin2.svg",
+    "name": "Pin2"
+  },
+  {
+    "id": 41,
+    "src": "/img/tiles/Pin2.svg",
+    "name": "Pin2"
+  },
+  {
+    "id": 42,
+    "src": "/img/tiles/Pin2.svg",
+    "name": "Pin2"
+  },
+  {
+    "id": 43,
+    "src": "/img/tiles/Pin2.svg",
+    "name": "Pin2"
+  },
+  {
+    "id": 44,
+    "src": "/img/tiles/Pin3.svg",
+    "name": "Pin3"
+  },
+  {
+    "id": 45,
+    "src": "/img/tiles/Pin3.svg",
+    "name": "Pin3"
+  },
+  {
+    "id": 46,
+    "src": "/img/tiles/Pin3.svg",
+    "name": "Pin3"
+  },
+  {
+    "id": 47,
+    "src": "/img/tiles/Pin3.svg",
+    "name": "Pin3"
+  },
+  {
+    "id": 48,
+    "src": "/img/tiles/Pin4.svg",
+    "name": "Pin4"
+  },
+  {
+    "id": 49,
+    "src": "/img/tiles/Pin4.svg",
+    "name": "Pin4"
+  },
+  {
+    "id": 50,
+    "src": "/img/tiles/Pin4.svg",
+    "name": "Pin4"
+  },
+  {
+    "id": 51,
+    "src": "/img/tiles/Pin4.svg",
+    "name": "Pin4"
+  },
+  {
+    "id": 52,
+    "src": "/img/tiles/Pin5.svg",
+    "name": "Pin5"
+  },
+  {
+    "id": 53,
+    "src": "/img/tiles/Pin5.svg",
+    "name": "Pin5"
+  },
+  {
+    "id": 54,
+    "src": "/img/tiles/Pin5.svg",
+    "name": "Pin5"
+  },
+  {
+    "id": 55,
+    "src": "/img/tiles/Pin5-Dora.svg",
+    "name": "Pin5-Dora"
+  },
+  {
+    "id": 56,
+    "src": "/img/tiles/Pin6.svg",
+    "name": "Pin6"
+  },
+  {
+    "id": 57,
+    "src": "/img/tiles/Pin6.svg",
+    "name": "Pin6"
+  },
+  {
+    "id": 58,
+    "src": "/img/tiles/Pin6.svg",
+    "name": "Pin6"
+  },
+  {
+    "id": 59,
+    "src": "/img/tiles/Pin6.svg",
+    "name": "Pin6"
+  },
+  {
+    "id": 60,
+    "src": "/img/tiles/Pin7.svg",
+    "name": "Pin7"
+  },
+  {
+    "id": 61,
+    "src": "/img/tiles/Pin7.svg",
+    "name": "Pin7"
+  },
+  {
+    "id": 62,
+    "src": "/img/tiles/Pin7.svg",
+    "name": "Pin7"
+  },
+  {
+    "id": 63,
+    "src": "/img/tiles/Pin7.svg",
+    "name": "Pin7"
+  },
+  {
+    "id": 64,
+    "src": "/img/tiles/Pin8.svg",
+    "name": "Pin8"
+  },
+  {
+    "id": 65,
+    "src": "/img/tiles/Pin8.svg",
+    "name": "Pin8"
+  },
+  {
+    "id": 66,
+    "src": "/img/tiles/Pin8.svg",
+    "name": "Pin8"
+  },
+  {
+    "id": 67,
+    "src": "/img/tiles/Pin8.svg",
+    "name": "Pin8"
+  },
+  {
+    "id": 68,
+    "src": "/img/tiles/Pin9.svg",
+    "name": "Pin9"
+  },
+  {
+    "id": 69,
+    "src": "/img/tiles/Pin9.svg",
+    "name": "Pin9"
+  },
+  {
+    "id": 70,
+    "src": "/img/tiles/Pin9.svg",
+    "name": "Pin9"
+  },
+  {
+    "id": 71,
+    "src": "/img/tiles/Pin9.svg",
+    "name": "Pin9"
+  },
+  {
+    "id": 72,
+    "src": "/img/tiles/Sou1.svg",
+    "name": "Sou1"
+  },
+  {
+    "id": 73,
+    "src": "/img/tiles/Sou1.svg",
+    "name": "Sou1"
+  },
+  {
+    "id": 74,
+    "src": "/img/tiles/Sou1.svg",
+    "name": "Sou1"
+  },
+  {
+    "id": 75,
+    "src": "/img/tiles/Sou1.svg",
+    "name": "Sou1"
+  },
+  {
+    "id": 76,
+    "src": "/img/tiles/Sou2.svg",
+    "name": "Sou2"
+  },
+  {
+    "id": 77,
+    "src": "/img/tiles/Sou2.svg",
+    "name": "Sou2"
+  },
+  {
+    "id": 78,
+    "src": "/img/tiles/Sou2.svg",
+    "name": "Sou2"
+  },
+  {
+    "id": 79,
+    "src": "/img/tiles/Sou2.svg",
+    "name": "Sou2"
+  },
+  {
+    "id": 80,
+    "src": "/img/tiles/Sou3.svg",
+    "name": "Sou3"
+  },
+  {
+    "id": 81,
+    "src": "/img/tiles/Sou3.svg",
+    "name": "Sou3"
+  },
+  {
+    "id": 82,
+    "src": "/img/tiles/Sou3.svg",
+    "name": "Sou3"
+  },
+  {
+    "id": 83,
+    "src": "/img/tiles/Sou3.svg",
+    "name": "Sou3"
+  },
+  {
+    "id": 84,
+    "src": "/img/tiles/Sou4.svg",
+    "name": "Sou4"
+  },
+  {
+    "id": 85,
+    "src": "/img/tiles/Sou4.svg",
+    "name": "Sou4"
+  },
+  {
+    "id": 86,
+    "src": "/img/tiles/Sou4.svg",
+    "name": "Sou4"
+  },
+  {
+    "id": 87,
+    "src": "/img/tiles/Sou4.svg",
+    "name": "Sou4"
+  },
+  {
+    "id": 88,
+    "src": "/img/tiles/Sou5.svg",
+    "name": "Sou5"
+  },
+  {
+    "id": 89,
+    "src": "/img/tiles/Sou5.svg",
+    "name": "Sou5"
+  },
+  {
+    "id": 90,
+    "src": "/img/tiles/Sou5.svg",
+    "name": "Sou5"
+  },
+  {
+    "id": 91,
+    "src": "/img/tiles/Sou5-Dora.svg",
+    "name": "Sou5-Dora"
+  },
+  {
+    "id": 92,
+    "src": "/img/tiles/Sou6.svg",
+    "name": "Sou6"
+  },
+  {
+    "id": 93,
+    "src": "/img/tiles/Sou6.svg",
+    "name": "Sou6"
+  },
+  {
+    "id": 94,
+    "src": "/img/tiles/Sou6.svg",
+    "name": "Sou6"
+  },
+  {
+    "id": 95,
+    "src": "/img/tiles/Sou6.svg",
+    "name": "Sou6"
+  },
+  {
+    "id": 96,
+    "src": "/img/tiles/Sou7.svg",
+    "name": "Sou7"
+  },
+  {
+    "id": 97,
+    "src": "/img/tiles/Sou7.svg",
+    "name": "Sou7"
+  },
+  {
+    "id": 98,
+    "src": "/img/tiles/Sou7.svg",
+    "name": "Sou7"
+  },
+  {
+    "id": 99,
+    "src": "/img/tiles/Sou7.svg",
+    "name": "Sou7"
+  },
+  {
+    "id": 100,
+    "src": "/img/tiles/Sou8.svg",
+    "name": "Sou8"
+  },
+  {
+    "id": 101,
+    "src": "/img/tiles/Sou8.svg",
+    "name": "Sou8"
+  },
+  {
+    "id": 102,
+    "src": "/img/tiles/Sou8.svg",
+    "name": "Sou8"
+  },
+  {
+    "id": 103,
+    "src": "/img/tiles/Sou8.svg",
+    "name": "Sou8"
+  },
+  {
+    "id": 104,
+    "src": "/img/tiles/Sou9.svg",
+    "name": "Sou9"
+  },
+  {
+    "id": 105,
+    "src": "/img/tiles/Sou9.svg",
+    "name": "Sou9"
+  },
+  {
+    "id": 106,
+    "src": "/img/tiles/Sou9.svg",
+    "name": "Sou9"
+  },
+  {
+    "id": 107,
+    "src": "/img/tiles/Sou9.svg",
+    "name": "Sou9"
+  },
+  {
+    "id": 108,
+    "src": "/img/tiles/Ton.svg",
+    "name": "Ton"
+  },
+  {
+    "id": 109,
+    "src": "/img/tiles/Ton.svg",
+    "name": "Ton"
+  },
+  {
+    "id": 110,
+    "src": "/img/tiles/Ton.svg",
+    "name": "Ton"
+  },
+  {
+    "id": 111,
+    "src": "/img/tiles/Ton.svg",
+    "name": "Ton"
+  },
+  {
+    "id": 112,
+    "src": "/img/tiles/Nan.svg",
+    "name": "Nan"
+  },
+  {
+    "id": 113,
+    "src": "/img/tiles/Nan.svg",
+    "name": "Nan"
+  },
+  {
+    "id": 114,
+    "src": "/img/tiles/Nan.svg",
+    "name": "Nan"
+  },
+  {
+    "id": 115,
+    "src": "/img/tiles/Nan.svg",
+    "name": "Nan"
+  },
+  {
+    "id": 116,
+    "src": "/img/tiles/Shaa.svg",
+    "name": "Shaa"
+  },
+  {
+    "id": 117,
+    "src": "/img/tiles/Shaa.svg",
+    "name": "Shaa"
+  },
+  {
+    "id": 118,
+    "src": "/img/tiles/Shaa.svg",
+    "name": "Shaa"
+  },
+  {
+    "id": 119,
+    "src": "/img/tiles/Shaa.svg",
+    "name": "Shaa"
+  },
+  {
+    "id": 120,
+    "src": "/img/tiles/Pei.svg",
+    "name": "Pei"
+  },
+  {
+    "id": 121,
+    "src": "/img/tiles/Pei.svg",
+    "name": "Pei"
+  },
+  {
+    "id": 122,
+    "src": "/img/tiles/Pei.svg",
+    "name": "Pei"
+  },
+  {
+    "id": 123,
+    "src": "/img/tiles/Pei.svg",
+    "name": "Pei"
+  },
+  {
+    "id": 124,
+    "src": "/img/tiles/Hatsu.svg",
+    "name": "Hatsu"
+  },
+  {
+    "id": 125,
+    "src": "/img/tiles/Hatsu.svg",
+    "name": "Hatsu"
+  },
+  {
+    "id": 126,
+    "src": "/img/tiles/Hatsu.svg",
+    "name": "Hatsu"
+  },
+  {
+    "id": 127,
+    "src": "/img/tiles/Hatsu.svg",
+    "name": "Hatsu"
+  },
+  {
+    "id": 128,
+    "src": "/img/tiles/Chun.svg",
+    "name": "Chun"
+  },
+  {
+    "id": 129,
+    "src": "/img/tiles/Chun.svg",
+    "name": "Chun"
+  },
+  {
+    "id": 130,
+    "src": "/img/tiles/Chun.svg",
+    "name": "Chun"
+  },
+  {
+    "id": 131,
+    "src": "/img/tiles/Chun.svg",
+    "name": "Chun"
+  },
+  {
+    "id": 132,
+    "src": "/img/tiles/Haku.svg",
+    "name": "Haku"
+  },
+  {
+    "id": 133,
+    "src": "/img/tiles/Haku.svg",
+    "name": "Haku"
+  },
+  {
+    "id": 134,
+    "src": "/img/tiles/Haku.svg",
+    "name": "Haku"
+  },
+  {
+    "id": 135,
+    "src": "/img/tiles/Haku.svg",
+    "name": "Haku"
+  }
 ]
