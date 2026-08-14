162e677ba95fdc1a6b26054a83ff99eb2d16d9ca
Vanesa Robledo <v.robledo@vehikl.com>
2026-08-14 00:38:35 -0400

    extract useDraggable into a component

---
+++ src/components/TileDraggable.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {useDraggable} from "@vueuse/core";
+import {useTemplateRef} from "vue";
+
+const props = defineProps<{
+  initialX: number
+  initialY: number
+  containerRef: HTMLElement | null
+}>()
+
+const el = useTemplateRef('el')
+const { x, y, style } = useDraggable(el, {
+  initialValue: { x: props.initialX, y: props.initialY },
+  containerElement: () => props.containerRef,
+  preventDefault: true,
+})
+
+</script>
+
+<template>
+  <div ref="el" :style="style" class="tile-wrapper">
+    <slot />
+  </div>
+</template>=
